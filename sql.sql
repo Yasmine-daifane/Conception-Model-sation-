34e3a1c36e9c4e7bcf38c6e665762b95118336c7
yasmine <110662044+YAS15mine@users.noreply.github.com>
2023-02-17 01:39:00 -0800

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,38 +1,38 @@
-CREATE DATABASE my_briefe ;
+CREATE DATABASE Homes_Agency;
 
-USE my_briefe ;
+USE HomesAgency ;
 
-CREATE TABLE Client_(
-   N__de_client_ INT,
+CREATE TABLE Client(
+   Ndeclient INT,
    nom VARCHAR(50),
    prénom VARCHAR(50),
    adresse_email VARCHAR(100),
    mot_de_passe VARCHAR(100),
    téléphone VARCHAR(20),
-   PRIMARY KEY(N__de_client_)
+   PRIMARY KEY(Ndeclient)
 );
 
 CREATE TABLE Annonce(
-   N__d_annonce INT,
-   titre_ VARCHAR(100),
+   Ndannonce INT,
+   titre VARCHAR(100),
    prix VARCHAR(50),
-   date_de_publication_ VARCHAR(50),
-   _date_de_dernière__modification_ VARCHAR(50),
+   date_depublication VARCHAR(50),
+   _date_de_dernièremodification_ VARCHAR(50),
    N_rue VARCHAR(50),
    nom_de_rue VARCHAR(50),
    ville VARCHAR(50),
    pays VARCHAR(50),
    code_postale VARCHAR(50),
-   N__de_client_ INT NOT NULL,
-   PRIMARY KEY(N__d_annonce),
-   FOREIGN KEY(N__de_client_) REFERENCES Client_(N__de_client_)
+   Ndeclient INT NOT NULL,
+   PRIMARY KEY(Nd_annonce),
+   FOREIGN KEY(Ndeclient) REFERENCES Client_(Ndeclient)
 );
 
 CREATE TABLE Images(
    Id_Images int NOT NULL AUTO_INCREMENT,
    is_pricipale BOOLEAN NOT NULL,
    image_path VARCHAR(50),
-   N__d_annonce INT NOT NULL,
+   Nd_annonce INT NOT NULL,
    PRIMARY KEY(Id_Images),
-   FOREIGN KEY(N__d_annonce) REFERENCES Annonce(N__d_annonce)
+   FOREIGN KEY(Nd_annonce) REFERENCES Annonce(N__d_annonce)
 );
